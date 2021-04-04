e3c38c5da10cf39f5976e34b962bc127b7a2e0fc
Thomas kimberly <thomasklfs@gmail.com>
2021-04-04 17:26:03 -0300

    base da calculadora

--- RestWithASPNETUdemy/RestWithASPNETUdemy/obj/Debug/net5.0/RestWithASPNETUdemy.AssemblyInfo.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/obj/Debug/net5.0/RestWithASPNETUdemy.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("99c046eb-aba8-469b-8dc8-c8ae48cc3946")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("56b2883a-ce77-435d-9b8e-d4b186f3e7a3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestWithASPNETUdemy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

f0785632428de51cf2129fbbd712e8ba8255062f
Thomas kimberly <thomasklfs@gmail.com>
2021-04-05 02:48:07 -0300

    operaçãoes básicas da calculadora

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -19,33 +19,101 @@ namespace RestWithASPNETUdemy.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
-                var sum = ConverToDecimal(firstNumber) + ConverToDecimal(secondNumber);
-                return Ok(sum.ToString());
+                var result = ConverToDecimal(firstNumber) + ConverToDecimal(secondNumber);
+                return Ok(result.ToString());
             }
             return BadRequest("Invalid input");
         }
 
-        private decimal ConverToDecimal(string strNumber)
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
         {
-            decimal decimalValue;
-            if (decimal.TryParse(strNumber, out decimalValue))
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var result = ConverToDecimal(firstNumber) - ConverToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var result = ConverToDecimal(firstNumber) * ConverToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var result = ConverToDecimal(firstNumber) / ConverToDecimal(secondNumber);
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public IActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var result = (ConverToDecimal(firstNumber) + ConverToDecimal(secondNumber)) / 2;
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("squareRoot/{value}")]
+        public IActionResult SquareRoot(string value)
+        {
+            if (IsNumeric(value))
+            {
+                var result = Math.Sqrt((double)ConverToDecimal(value));
+                return Ok(result.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        [HttpGet("imc/{weight}/{height}")]
+        public IActionResult IMC(string weight, string height)
+        {
+            if (IsNumeric(weight) && IsNumeric(height))
+            {
+                var result = ConverToDecimal(weight) / (ConverToDecimal(height) * ConverToDecimal(height));
+                return Ok("Magreza, quando o resultado é menor que 18,5 kg/m2" + Environment.NewLine
+                    + "Normal, quando o resultado está entre 18, 5 e 24, 9 kg/m2" + Environment.NewLine
+                    + "Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2" + Environment.NewLine
+                    + "Obesidade, quando o resultado é maior que 30 kg/m2" + Environment.NewLine 
+                    + $"Resultado: {result}");
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private static decimal ConverToDecimal(string strNumber)
+        {
+            if (decimal.TryParse(strNumber, out decimal decimalValue))
             {
                 return decimalValue;
             }
             return 0;
         }
 
-        private bool IsNumeric(string strNumber)
+        private static bool IsNumeric(string strNumber)
         {
-            double number;
             bool isNumber = double.TryParse(strNumber,
                 System.Globalization.NumberStyles.Any,
                 System.Globalization.NumberFormatInfo.InvariantInfo,
-                out number);
+                out _);
             return isNumber;
         }
     }
